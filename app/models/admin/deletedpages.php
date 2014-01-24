1f843357e304337f5233e763c08ecba2427c2db7
Igor.Vasinsky <Igor.Vasinsky@gmail.com>
2014-01-24 04:49:08 +0000

    создан метод, перенесён запрос из контроллера

--- app/models/admin/deletedpages.php
+++ app/models/admin/deletedpages.php
@@ -3,6 +3,12 @@
         public function __construct(){
             parent::__construct();
         }
+        public function getListPages(){
+           $result = $this->getPaginateData("select * from pages p left join section s 
+           on p.sid = s.sid where p.display = 3 order by p.sid , pid DESC", 25,CUR_PAGE);
+           
+           return $result; 
+        }
         
         public function deletePage($pid){
             $sql = "update pages set display=3 where pid=".(int)$pid;
